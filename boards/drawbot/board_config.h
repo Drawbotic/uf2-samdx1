e49206b07112887e80d70c0796a23780643ac0fe
Elliott Wilson <elliott.wilson@monash.edu>
2020-08-24 19:32:25 +1000

    Added DrawBot board defines

---
+++ boards/drawbot/board_config.h
@@ -0,0 +1,19 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "SensiLab"
+#define PRODUCT_NAME "DrawBot"
+#define VOLUME_LABEL "DRAWBOT"
+#define INDEX_URL "https://sensilab.monash.edu"
+#define BOARD_ID "SAMD21J18A-DrawBot"
+
+#define USB_VID 0x1209
+#define USB_PID 0x0001
+
+#define LED_PIN PIN_PB07
+
+#define BOARD_NEOPIXEL_PIN PIN_PA08
+#define BOARD_NEOPIXEL_COUNT 8
+
+
+#endif
